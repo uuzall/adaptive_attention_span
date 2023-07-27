00c263a9928cffd9afa4af2f659b398d4e349697
uzal <uzal.chhetri@live.com>
2023-07-27 21:55:08 +0600

    experiments with attention spans

--- data.py
+++ data.py
@@ -34,6 +34,3 @@ def dataloader(split):
 	x = torch.stack([data[i:i+block_size] for i in ix])
 	y = torch.stack([data[i+1:i+block_size+1] for i in ix])
 	return x, y 
-
-def decode(x): 
-	return decode(x)
